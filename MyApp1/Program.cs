c4b52e21c08980f00adeb23b26d51c8dda80871e
willyamsann <willyam.oliveira@dellead.com>
2021-11-04 14:01:11 -0300

    Add Methods

--- MyApp1/Program.cs
+++ MyApp1/Program.cs
@@ -6,7 +6,23 @@ namespace MyApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dell");
+            Console.WriteLine("Calculadora primeiro projeto");
+
+            //Dois padrões que vamos utilizar
+            //Normal: Willyam Santos de Oliveira
+            //CamelCase: WillyamSantosDeOliveira
+
+            //Regra que sempre vamos utilizar nome de variael sempre começa com minusculo
+            int numberOne = Int32.Parse(Console.ReadLine());
+            int numberTwo = Int32.Parse(Console.ReadLine());
+
+            int soma = numberOne + numberTwo;
+            int multiplcao = numberOne * numberTwo;
+            var divisao = numberOne / numberTwo;
+
+            Console.WriteLine("A Soma foi:" + soma);
+            Console.WriteLine("A Multiplicação foi:" + multiplcao);
+            Console.WriteLine("A Divisão foi:" + divisao);
         }
     }
 }
