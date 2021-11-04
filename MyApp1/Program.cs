e0b36dba4fa4f62e77c41a08011fe6067378b382
willyamsann <willyam.oliveira@dellead.com>
2021-11-04 13:17:45 -0300

    Change Name

--- MyApp1/Program.cs
+++ MyApp1/Program.cs
@@ -6,7 +6,7 @@ namespace MyApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dell");
         }
     }
 }
